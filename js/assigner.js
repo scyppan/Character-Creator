62e3ebca41dcb228340606254973c97828a558a2
scyppan <adam@scyppan.com>
2025-05-16 16:26:47 -0500

    Update assigner.js

--- js/assigner.js
+++ js/assigner.js
@@ -1,83 +1,97 @@
 const assignments = [
-  // Identity
-  { selector: '#frm_field_7510_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7571_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7551_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7554_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7553_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7555_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7556_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7557_container', panelId: 'panel-identity' },
-  { selector: '#frm_field_7558_container', panelId: 'panel-identity' },
-  
+    // Identity
+    { selector: '#frm_field_7510_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7571_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7551_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7554_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7553_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7555_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7556_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7557_container', panelId: 'panel-identity' },
+    { selector: '#frm_field_7558_container', panelId: 'panel-identity' },
 
-  // Parentage (with Birthday section)
-  { selector: '#frm_field_7789_container', panelId: 'panel-parentage' },
-  { selector: '#frm_field_7787_container', panelId: 'panel-parentage' },
-  { selector: '#frm_field_7799_container', panelId: 'panel-parentage' },
-  { selector: '#frm_field_7565_container', panelId: 'panel-parentage' },
 
-  // Initial Interests
-  { selector: '#frm_field_7791_container', panelId: 'panel-initial-interests' },
+    // Parentage (with Birthday section)
+    { selector: '#frm_field_7789_container', panelId: 'panel-parentage' },
+    { selector: '#frm_field_7787_container', panelId: 'panel-parentage' },
+    { selector: '#frm_field_7799_container', panelId: 'panel-parentage' },
+    { selector: '#frm_field_7565_container', panelId: 'panel-parentage' },
 
-  // Equipment
-  { selector: '#frm_field_7746_container', panelId: 'panel-equipment' },
-  { selector: '#frm_field_8635_container', panelId: 'panel-equipment' },
-  { selector: '#frm_field_7735_container', panelId: 'panel-equipment' },
-  { selector: '#frm_field_7727_container', panelId: 'panel-equipment' },
-  { selector: '#frm_field_7731_container', panelId: 'panel-equipment' },
-  { selector: '#frm_field_8650_container', panelId: 'panel-equipment' },
+    // Initial Interests
+    { selector: '#frm_field_7791_container', panelId: 'panel-initial-interests' },
 
-  // Relationships with Creatures
-  { selector: '#frm_field_8060_container', panelId: 'panel-relationships-creatures' },
+    // Equipment
+    { selector: '#frm_field_7746_container', panelId: 'panel-equipment' },
+    { selector: '#frm_field_8635_container', panelId: 'panel-equipment' },
+    { selector: '#frm_field_7735_container', panelId: 'panel-equipment' },
+    { selector: '#frm_field_7727_container', panelId: 'panel-equipment' },
+    { selector: '#frm_field_7731_container', panelId: 'panel-equipment' },
+    { selector: '#frm_field_8650_container', panelId: 'panel-equipment' },
 
-  // Inventory
-  { selector: '#frm_field_7728_container', panelId: 'panel-inventory' },
+    // Relationships with Creatures
+    { selector: '#frm_field_8060_container', panelId: 'panel-relationships-creatures' },
 
-  // Spells
-  { selector: '#frm_field_7748_container', panelId: 'panel-spells' },
+    // Inventory
+    { selector: '#frm_field_7728_container', panelId: 'panel-inventory' },
 
-  // Proficiencies
-  { selector: '#frm_field_7838_container', panelId: 'panel-proficiencies' },
+    // Spells
+    { selector: '#frm_field_7748_container', panelId: 'panel-spells' },
 
-  // Potion Knowledge
-  { selector: '#frm_field_8047_container', panelId: 'panel-potion-knowledge' },
+    // Proficiencies
+    { selector: '#frm_field_7838_container', panelId: 'panel-proficiencies' },
 
-  // Relationships with Humans
-  { selector: '#frm_section_7779-0', panelId: 'panel-relationships-humans' },
+    // Potion Knowledge
+    { selector: '#frm_field_8047_container', panelId: 'panel-potion-knowledge' },
 
-  // Notes
-  { selector: '#frm_field_8408_container', panelId: 'panel-notes' },
-  { selector: '#frm_field_7776_container', panelId: 'panel-notes' },
+    // Relationships with Humans
+    { selector: '#frm_section_7779-0', panelId: 'panel-relationships-humans' },
 
-  // Personality
-  { selector: '#frm_field_7650_container', panelId: 'panel-personality' },
+    // Notes
+    { selector: '#frm_field_8408_container', panelId: 'panel-notes' },
+    { selector: '#frm_field_7776_container', panelId: 'panel-notes' },
 
-  // Characteristics
-  { selector: '#frm_field_7585_container', panelId: 'panel-characteristics' },
+    // Personality
+    { selector: '#frm_field_7650_container', panelId: 'panel-personality' },
 
-  // Education
-  { selector: '#frm_field_7511_container', panelId: 'panel-education' },
+    // Characteristics
+    { selector: '#frm_field_7585_container', panelId: 'panel-characteristics' },
 
-  // Eminence
-  { selector: '#frm_field_7671_container', panelId: 'panel-eminence' }
+    // Education
+    { selector: '#frm_field_7511_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7511_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7515_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7514_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7609_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7608_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7615_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7616_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7622_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7623_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7630_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7629_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7636_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7637_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7643_container', panelId: 'panel-education' },
+    { selector: '#frm_field_7644_container', panelId: 'panel-education' },
+
+
+    // Eminence
+    { selector: '#frm_field_7671_container', panelId: 'panel-eminence' }
 ];
-
-
 
 // 2) move existing elements into your panels
 const assignElementsToPanels = () => {
-  assignments.forEach(({ selector, panelId }) => {
-    const elem  = document.querySelector(selector);
-    const panel = document.getElementById(panelId);
-    if (elem && panel) panel.appendChild(elem);
-  });
+    assignments.forEach(({ selector, panelId }) => {
+        const elem = document.querySelector(selector);
+        const panel = document.getElementById(panelId);
+        if (elem && panel) panel.appendChild(elem);
+    });
 };
 
 const deferMove = (selector, panelId) => {
-  window.addEventListener('load', () => {
-    const el    = document.querySelector(selector);
-    const panel = document.getElementById(panelId);
-    if (el && panel) panel.appendChild(el);
-  });
+    window.addEventListener('load', () => {
+        const el = document.querySelector(selector);
+        const panel = document.getElementById(panelId);
+        if (el && panel) panel.appendChild(el);
+    });
 };
