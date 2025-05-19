78a2f6fc322389a3b0fb2e4b31ec7ab357587aa6
scyppan <adam@scyppan.com>
2025-05-19 18:38:10 -0500

    fixing livename and panel top

--- js/1assignments/livename.js
+++ js/1assignments/livename.js
@@ -1,6 +1,6 @@
 function syncEntryTitle() {
   const inputEl = document.getElementById('field_5syv4');
-  const titleEl = document.querySelector('h2.entry-title');
+  const titleEl = document.querySelector('h2.wp-block-post-title');
   if (!inputEl || !titleEl) return;
 
   inputEl.addEventListener('input', () => {
